f776a4881df6476cb39ff91de73ffd2c8a8fb934
Alberto Lazari <alberto.lazari@gmail.com>
2022-05-22 14:19:19 +0200

    User validations

--- src/main/java/it/unipd/mtss/model/User.java
+++ src/main/java/it/unipd/mtss/model/User.java
@@ -6,14 +6,29 @@
 package it.unipd.mtss.model;
 
 public class User {
+    private final String name;
+    private final int age;
+
     public User(String name, int age) {
+        if (name == null) {
+            throw new IllegalArgumentException("Il nome non può essere nullo");
+        }
+        if (age < 0) {
+            throw new IllegalArgumentException("L'età non può essere negativa");
+        }
+        if (name.equals("")) {
+            throw new IllegalArgumentException("Il nome non può essere vuoto");
+        }
+
+        this.name = name;
+        this.age = age;
     }
 
     public String getName() {
-        return "";
+        return name;
     }
 
     public int getAge() {
-        return 0;
+        return age;
     }
 }
